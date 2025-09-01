7d100d8d8bde49350d1129792b8327c40c3de199
Johan_31 <johandelgadovasquez@gmail.com>
2025-09-01 12:45:43 -0500

    bucle

--- tarea_semana_3.py
+++ tarea_semana_3.py
@@ -1,6 +1,13 @@
+#generamos la lista de 10 aleatorios
 import random as rd
 lisT = []
 for i in range(10):
     num = rd.randint(1, 10)
     lisT.append(num)
-print(lisT)
+print("Lista original:",lisT)
+#realizamos el bucle
+for i in range(len(lisT)):
+  for j in range(len(lisT)-1):
+    if lisT[j] > lisT[j+1]:
+       #entonces realizamos el cambio
+       lisT[j], lisT[j+1] = lisT[j+1], lisT[j]
