361435b69d4ab223d4bf67d1b2e92743201d2566
Johan_31 <johandelgadovasquez@gmail.com>
2025-09-01 12:55:00 -0500

    imprimimos y ya

--- tarea_semana_3.py
+++ tarea_semana_3.py
@@ -5,9 +5,19 @@ for i in range(10):
     num = rd.randint(1, 10)
     lisT.append(num)
 print("Lista original:",lisT)
-#realizamos el bucle
+#realizamos el bucle para ordenar la lista de forma ascendente
 for i in range(len(lisT)):
   for j in range(len(lisT)-1):
     if lisT[j] > lisT[j+1]:
        #entonces realizamos el cambio
        lisT[j], lisT[j+1] = lisT[j+1], lisT[j]
+#ordenamos la lista de forma descedente
+lista_des = lisT.copy()
+for i in range(len(lista_des)):
+   for j in range(len(lista_des)-1):
+     if lista_des[j] < lista_des[j+1]:
+        #intercambiamos
+        lista_des[j], lista_des[j+1] = lista_des[j+1], lista_des[j]
+
+print("lista Ascendente:", lisT)
+print("lista Descendente:", lista_des)
